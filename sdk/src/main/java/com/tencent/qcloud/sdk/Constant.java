2db0026793ad9ba3fa2db1f5f9c82e1d7845ce62
huangyuefeng <mcxinyu@foxmail.com>
2018-08-09 20:47:00 +0800

    tim2.7.1

---
+++ sdk/src/main/java/com/tencent/qcloud/sdk/Constant.java
@@ -0,0 +1,14 @@
+package com.tencent.qcloud.sdk;
+
+/**
+ * 常量
+ */
+public class Constant {
+
+    // public static final int ACCOUNT_TYPE = 792;
+    public static final int ACCOUNT_TYPE = 28845;
+    //sdk appid 由腾讯分配
+    // public static final int SDK_APPID = 1400001533;
+    public static final int SDK_APPID = 1400101401;
+
+}
